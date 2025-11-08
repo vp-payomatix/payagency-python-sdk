d97061eb3150b1ed3a2dbaf0e9413398c676c364
vp-payomatix <vipin@payomatix.com>
2025-11-08 11:51:30 +0530

    feat: Successfully deploy PayAgency Python SDK v1.1.0 to PyPI
    - Updated version to v1.1.0 to comply with PyPI reuse restrictions
    - Package successfully uploaded and verified on PyPI
    - Added deployment script for future releases
    - All tests passing and package integrity confirmed
    - Available for installation: pip install payagency-api==1.1.0
    
     PayAgency Python SDK now publicly available on PyPI!

--- payagency_api/__init__.py
+++ payagency_api/__init__.py
@@ -8,7 +8,7 @@ from .client import PayAgencyApi
 from .exceptions import PayAgencyError, PayAgencyAPIError, PayAgencyNetworkError
 from . import types
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __author__ = "PaneruVipin"
 __email__ = "support@pay.agency"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="payagency-api",
-    version="1.0.1",
+    version="1.1.0",
     author="PaneruVipin",
     author_email="support@pay.agency",
     description="A comprehensive Python SDK for PayAgency payment processing platform",
