38dcbb2e370fd37b8cfbb3118a8a45450e70a0b9
vp-payomatix <vipin@payomatix.com>
2025-11-08 11:41:12 +0530

    feat: Update to v1.0.1 for PyPI deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="payagency-api",
-    version="1.0.0",
+    version="1.0.1",
     author="PaneruVipin",
     author_email="support@pay.agency",
     description="A comprehensive Python SDK for PayAgency payment processing platform",
